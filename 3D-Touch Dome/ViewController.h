3c3236b8341cca3cd381108a220e7cd912f5eb6c
FighterRay27 <554269861@qq.com>
2015-12-02 22:58:59 +0800

    first commit

---
+++ 3D-Touch Dome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3D-Touch Dome
+//
+//  Created by 张润峰 on 15/12/2.
+//  Copyright © 2015年 张润峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3D-Touch Dome/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  3D-Touch Dome
+//
+//  Created by 张润峰 on 15/12/2.
+//  Copyright © 2015年 张润峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
